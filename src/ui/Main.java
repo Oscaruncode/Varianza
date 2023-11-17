2a22aeececad9e310f1c6c3c392921d3baae7806
Oscaruncode <oscarfuentesperezosp@gmail.com>
2023-11-17 13:40:31 -0500

    Santiago problema eee

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main( String[] pArgs ){
             GraphicInferfaz interfaz = new GraphicInferfaz( );
             interfaz.setVisible( true );
-        System.out.println("hello world");
+        System.out.println("hello world Santiago");
     }
 }
 
