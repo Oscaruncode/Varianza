dde638c2bf7be5dc8805b7fa50f796385b36deef
Oscaruncode <oscarfuentesperezosp@gmail.com>
2023-11-17 13:40:06 -0500

    Before sebastian

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -7,5 +7,9 @@ public class Main {
     public static void main( String[] pArgs ){
             GraphicInferfaz interfaz = new GraphicInferfaz( );
             interfaz.setVisible( true );
+        System.out.println("hello world");
     }
 }
+
+
+//nuevo commit
